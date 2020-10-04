44fbb3d476e186016549f3a6cbddd5a4b7bc9613
ChinYa <smallrespect33@gmail.com>
2020-10-04 19:57:22 +0800

    Revert "解決companylist排版問題(f5會出問題不知為何)"
    This reverts commit a7e091f77d3adb35a38d3e6e9505a80fac4b97f4.

--- src/store/index.js
+++ src/store/index.js
@@ -6,20 +6,21 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     loginOrNot: false,
-    com_list: [],
+    com_list1: [],
+    com_list2: [],
+    com_list3: [] 
   },
   mutations: {
-    SETLIST(state, list){
-      for(var i = 0; i < list.length; i++){
-        state.com_list[i] = list[i][0]
-      }
+    SETLIST1(state, com_list1){
+      state.com_list1 = com_list1
     }
   },
   actions: {
-    GETLIST(context){
+    GETLIST1(context){
       console.log(context)
+      // context.commit('SETDAY')
       return axios.get('/com_data.json').then(res =>{
-        context.commit('SETLIST', res.data)
+        context.commit('SETLIST1', res.data)
       })
     }
   },
