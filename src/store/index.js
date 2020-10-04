a7e091f77d3adb35a38d3e6e9505a80fac4b97f4
ChinYa <smallrespect33@gmail.com>
2020-10-04 19:52:18 +0800

    解決companylist排版問題(f5會出問題不知為何)

--- src/store/index.js
+++ src/store/index.js
@@ -6,21 +6,20 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     loginOrNot: false,
-    com_list1: [],
-    com_list2: [],
-    com_list3: [] 
+    com_list: [],
   },
   mutations: {
-    SETLIST1(state, com_list1){
-      state.com_list1 = com_list1
+    SETLIST(state, list){
+      for(var i = 0; i < list.length; i++){
+        state.com_list[i] = list[i][0]
+      }
     }
   },
   actions: {
-    GETLIST1(context){
+    GETLIST(context){
       console.log(context)
-      // context.commit('SETDAY')
       return axios.get('/com_data.json').then(res =>{
-        context.commit('SETLIST1', res.data)
+        context.commit('SETLIST', res.data)
       })
     }
   },
